d2799bbff59d0a633ace54a66cae567fcfb8bdbc
zubier3 <bzubier437@gmail.com>
2024-10-25 13:14:35 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,1 @@
-echo "# UDatE_data71011" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/zubier3/UDatE_data71011.git
-git push -u origin main
+print("Hello from Binder!")
