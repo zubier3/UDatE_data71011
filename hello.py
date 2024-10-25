07232078c813fc9182b9060e7d21a679e340ba87
zubier3 <bzubier437@gmail.com>
2024-10-25 13:07:31 +0100

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,7 @@
+echo "# UDatE_data71011" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/zubier3/UDatE_data71011.git
+git push -u origin main
